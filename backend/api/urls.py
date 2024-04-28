7269480eeb96b4f4c3e63f69bf873e3a5f042ade
Kobelev Andrey <heiskobe@yandex.ru>
2024-04-28 14:07:23 +0300

    Add requirements

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter, DefaultRouter
+from rest_framework.routers import DefaultRouter
 
 from .views import (IngredientsViewSet, RecipeViewSet, TagsViewSet,
                     UsersViewSet, token_login, token_logout)
