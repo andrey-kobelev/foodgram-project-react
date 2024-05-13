1b994276332c9a39bfd578eeb79073e9a0d087dc
Kobelev Andrey <heiskobe@yandex.ru>
2024-05-13 22:51:57 +0300

    Fix user model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,42 +1,10 @@
 from django.conf import settings
-from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 from django.db import models
 
 from . import constants
 from .validators import username_validator, HexColorValidator
-
-
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
-        """
-        Create and save a user with the given email, and password.
-        """
-        if not email:
-            raise ValueError('The given email must be set')
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -63,7 +31,6 @@ class User(AbstractUser):
         verbose_name='Фамилия',
         max_length=settings.LAST_NAME_MAX_LENGTH
     )
-    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = [
